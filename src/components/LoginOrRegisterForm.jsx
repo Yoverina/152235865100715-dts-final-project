ff283bc6bd554c13b554d6f5d9017f44630a6a8c
Yoverina Nurdin <yoverina_nurdin@bca.co.id>
2022-08-04 19:17:02 +0700

    add loading for login/register page

--- src/components/LoginOrRegisterForm.jsx
+++ src/components/LoginOrRegisterForm.jsx
@@ -1,113 +1,154 @@
-import { Box, Button, Typography, TextField, Alert, AlertTitle } from "@mui/material";
+import {
+  Box,
+  Button,
+  Typography,
+  TextField,
+  Alert,
+  AlertTitle,
+} from "@mui/material";
 import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from 'react';
-import { useAuthState } from "react-firebase-hooks/auth"
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
 
-import { auth } from "../auth/firebase"
+import { auth } from "../auth/firebase";
+import Loading from "./Loading";
 
 const LoginOrRegisterForm = ({ action }) => {
   const navigate = useNavigate();
-  const [user, isLoading] = useAuthState(auth);
-  const [errorMsg, setErrorMsg] = useState('');
+  const [user, isLoading, isFetching] = useAuthState(auth);
+  const [errorMsg, setErrorMsg] = useState("");
 
-  useEffect(() =>{
-    if(isLoading){
-      //bisa return loading page disini
-      // return <Loading/>
-      return ;
-    }
-    if(user){
+  useEffect(() => {
+    if (user) {
       navigate("/");
     }
-  },
-  [user, isLoading, navigate]);
+  }, [user, navigate]);
 
-  const registerHandler = async(email, pass) =>{
+  const registerHandler = async (email, pass) => {
     try {
       await createUserWithEmailAndPassword(auth, email, pass);
-    } catch (error){
+    } catch (error) {
       setErrorMsg(error.message);
     }
   };
-  const loginHandler = async(email, pass) => {
+  const loginHandler = async (email, pass) => {
     try {
       await signInWithEmailAndPassword(auth, email, pass);
-    } catch (error){
+    } catch (error) {
       setErrorMsg(error.message);
     }
-
   };
   const buttonLoginOrRegisterOnClickHandler = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
-    const email = data.get('email');
-    const pass = data.get('password');
-    if(action === "login"){
+    const email = data.get("email");
+    const pass = data.get("password");
+    if (action === "login") {
       loginHandler(email, pass);
-    } else{
+    } else {
       registerHandler(email, pass);
     }
-  }
+  };
 
   return (
-    <Box width={400}
-    display="flex" flexDirection="column"
-    justifyContent="center"
-    minHeight="100vh" margin="auto">
-      <Typography component="h1" variant="h5">
-        {action === "login" ? "Login" : "Register Account"}
-      </Typography>
-      <Box component="form" onSubmit={buttonLoginOrRegisterOnClickHandler} noValidate sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          label="Email Address"
-          name="email"
-          autoFocus
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          label="Password"
-          type="password"
-          name="password"
-          autoComplete="current-password"
-        />
-        <Button type="submit"
-          fullWidth
-          variant="contained"
-          sx={{ backgroundColor: '#eb5e55', mt: 3, mb: 2 }}
+    <Box>
+      {(!user && (isLoading || isFetching)) ? (
+        <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+          <Loading />
+        </Box>
+      ) : (
+        <Box
+          width={400}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          minHeight="100vh"
+          margin="auto"
         >
-          {action === "login" ? "Login" : "Register Account"}
-        </Button>
-      </Box>
-      {errorMsg ? 
-        <Alert severity="error" style={{marginBottom: 20}}>
-          <AlertTitle><strong>ERROR</strong></AlertTitle>
-          {errorMsg}
-        </Alert>
-        : ""
-      }
-      {user ? 
-        <Alert severity="success" style={{marginBottom: 20}}>
-          <AlertTitle><strong>Success</strong></AlertTitle>
-          Loading...
-        </Alert>
-        : ""
-      }
-      {action === "login" ? 
-        (<Typography>
-          Don't have an account?
-          <Link style={{ textDecoration: 'none', marginLeft: 5}} to="/register" variant="body2">Sign Up</Link>
-        </Typography>
-        ) : <Typography>
-        Have an account?
-        <Link style={{textDecoration: 'none', marginLeft: 5}} to="/login" variant="body2">Login</Link>
-      </Typography>
-      }
+          <Typography component="h1" variant="h5">
+            {action === "login" ? "Login" : "Register Account"}
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={buttonLoginOrRegisterOnClickHandler}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Email Address"
+              name="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              label="Password"
+              type="password"
+              name="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ backgroundColor: "#eb5e55", mt: 3, mb: 2 }}
+            >
+              {action === "login" ? "Login" : "Register Account"}
+            </Button>
+          </Box>
+          {errorMsg ? (
+            <Alert severity="error" style={{ marginBottom: 20 }}>
+              <AlertTitle>
+                <strong>ERROR</strong>
+              </AlertTitle>
+              {errorMsg}
+            </Alert>
+          ) : (
+            ""
+          )}
+          {user ? (
+            <Alert severity="success" style={{ marginBottom: 20 }}>
+              <AlertTitle>
+                <strong>Success</strong>
+              </AlertTitle>
+              Loading...
+            </Alert>
+          ) : (
+            ""
+          )}
+          {action === "login" ? (
+            <Typography>
+              Don't have an account?
+              <Link
+                style={{ textDecoration: "none", marginLeft: 5 }}
+                to="/register"
+                variant="body2"
+              >
+                Sign Up
+              </Link>
+            </Typography>
+          ) : (
+            <Typography>
+              Have an account?
+              <Link
+                style={{ textDecoration: "none", marginLeft: 5 }}
+                to="/login"
+                variant="body2"
+              >
+                Login
+              </Link>
+            </Typography>
+          )}
+        </Box>
+      )}
     </Box>
   );
 };
