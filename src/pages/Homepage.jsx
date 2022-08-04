6dec5b0d0f733768f0e1c5dd08a7bbcbd00b56c0
Yoverina Nurdin <yoverina_nurdin@bca.co.id>
2022-08-04 13:50:24 +0700

    remove countrygraph from home

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,4 @@
 import { Box, Typography } from "@mui/material";
-import CountryHistoryGraph from "../components/CountryHistoryGraph";
 import TopFiveTotalCases from "../components/TopFiveTotalCases";
 import TopThreeNewCases from "../components/TopThreeNewCases";
 
@@ -16,7 +15,6 @@ const Homepage = () => {
         <Typography style={subtitle}>Top 5 New Cases Today</Typography>
         <Typography mb={2}>Data fetched: {new Date().toISOString().slice(0, 10)}</Typography>
         <TopThreeNewCases />
-        {/* <CountryHistoryGraph/> */}
       </Box>
       <Box mb={5}>
         <Typography style={subtitle}>Top 10 High Risk Country</Typography>
